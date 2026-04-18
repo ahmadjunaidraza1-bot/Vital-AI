5e007fd28cb9ddfc950f73d9a7f90f010b7f1f92
Junaid <ahmadjunaidraza1@gmail.com>
2026-04-18 14:33:34 +0500

    first

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          bootstrap: ['bootstrap', 'react-bootstrap', 'react-bootstrap-icons']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
+  }
 })
